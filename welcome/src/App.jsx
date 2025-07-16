f6c902826a1816d80cc07bb576f38289c7ebcf01
Paul Philips <paulphilips@email.com>
2025-07-16 23:20:37 +0530

    Button Part

---
+++ welcome/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MyButton = () => {
+  return <button>I'm a Button!</button>;
+};
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+};
+
+export default App;
